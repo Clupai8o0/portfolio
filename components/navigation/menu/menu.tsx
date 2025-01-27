d9a8e0b2af83293c6ed9bd9616c6a3d6fa4efdfd
Clupai8o0 <clupaio4@gmail.com>
2025-01-27 12:23:02 +1100

    after moving content

--- components/navigation/menu/menu.tsx
+++ components/navigation/menu/menu.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-// import Link from "../transition-link";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import Logo from "../logo";
-import { links } from "@/lib/links";
-import { generateKey } from "@/lib/utils";
+
+// import "./menu.css";
+
+import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+
+const menuLinks = [
+	{ path: "/", label: "Home" },
+	{ path: "/work", label: "Work" },
+	{ path: "/about", label: "About" },
+	{ path: "/contact", label: "Contact" },
+	{ path: "/lab", label: "Lab" },
+];
 
 const Menu = () => {
-	const container = useRef(null);
+	const container = useRef();
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	const tl = useRef(null);
+	const tl = useRef();
 
 	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
@@ -29,25 +36,19 @@ const Menu = () => {
 					clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
 					ease: "power4.inOut",
 				})
-				.fromTo(
-					".menu-link-item-holder",
-					{ y: 75 },
-					{
-						y: 100,
-						duration: 1,
-						stagger: 0.1,
-						ease: "power4.out",
-						delay: -0.75,
-						// delay: 2,
-					}
-				);
+				.to(".menu-link-item-holder", {
+					y: 0,
+					duration: 1,
+					stagger: 0.1,
+					ease: "power4.out",
+					delay: -0.75,
+				});
 		},
 		{ scope: container }
 	);
 
 	useEffect(() => {
 		if (isMenuOpen) {
-			console.log("animating");
 			tl.current.play();
 		} else {
 			tl.current.reverse();
@@ -84,8 +85,8 @@ const Menu = () => {
 				</div>
 				<div className="menu-copy">
 					<div className="menu-links">
-						{links.map((link) => (
-							<div key={generateKey()} className="menu-link-item">
+						{menuLinks.map((link, index) => (
+							<div key={index} className="menu-link-item">
 								<div className="menu-link-item-holder" onClick={toggleMenu}>
 									<Link className="menu-link" href={link.path}>
 										{link.label}
