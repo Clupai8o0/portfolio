2651ad9cf320219eab76066ba06e1570e76602fc
Clupai8o0 <clupaio4@gmail.com>
2025-02-08 14:39:52 +1100

    setting up text animations

--- components/content/about.tsx
+++ components/content/about.tsx
@@ -1,46 +1,95 @@
 "use client";
 
 import { generateKey } from "@/lib/utils";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Cover from "./cover";
 // import { ColourfulText } from "./colorful-text";
 import { HeartPop } from "./pop";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import GradientText from "./gradient-text";
 
+const aboutText = [
+	"Hi 👋, I'm Samridh Limbu. A creative developer that loves to make Innovative Solutions to real-world problems.",
+	"I'm skilled in developing websites and I'm constantly learning new technology.",
+	"Besides coding, I love reading, making art, and spending time with my loved ones.",
+];
+
+gsap.registerPlugin(ScrollTrigger);
 function About() {
+	const container = useRef(null);
+
+	useEffect(() => {
+		gsap.utils.toArray(".about-section").forEach((section: any, i) => {
+			const wrappers = section.querySelectorAll(".about-word");
+
+			wrappers.forEach((wrapper: any) => {
+				const word = wrapper.querySelector("span");
+
+				gsap.fromTo(
+					word,
+					{
+						y: 120,
+					},
+					{
+						y: 0,
+						duration: 0.5,
+						ease: "power1.out",
+						scrollTrigger: {
+							trigger: wrapper,
+							start: "top 60%",
+						},
+					}
+				);
+			});
+		});
+	}, []);
+
 	return (
 		<section
-			className="min-h-screen w-full p"
-			data-scroll
-			data-scroll-speed="0.3"
+			className="min-h-screen w-full p py-8 md:py-12 lg:py-20"
+			ref={container}
 		>
-			<div className="flex flex-wrap gap-2 md:gap-3 text-5xl md:text-6xl lg:text-8xl font-light">
-				{"Hi 👋, I'm Samridh Limbu. A creative developer that loves to make Innovative Solutions to real-world problems. I'm skilled in developing websites and I'm constantly learning new technology. Besides coding, I love reading, making art, and spending time with my loved ones."
-					.split(" ")
-					.map((word) => (
-						<div key={generateKey()} className="overflow-wrapper-2 relative">
-							<span>{word}</span>
-							{/* {word === "loved" && <HeartPop />}
-							{(() => {
-								// if (word === "creative")
-								// 	return <ColourfulText text="creative" />;
-								if (word === "Innovative") return <Cover>Innovative</Cover>;
-								if (word === "Solutions")
-									return <span className="text-solutions">Solutions</span>;
-								if (word === "problems.")
-									return (
-										<div className="italic">
-											<span>problems.</span>
-											<div className="holder clip">
-												<div className="circle circle1"></div>
-												<div className="circle circle2"></div>
-											</div>
-										</div>
-									);
+			<div className="flex flex-col gap-6 md:gap-10 text-5xl md:text-6xl lg:text-8xl font-light">
+				{aboutText.map((section, i) => (
+					<section
+						className={`about-section-${i} about-section w-full flex items-center flex-wrap justify-start gap-2 md:gap-3`}
+						key={generateKey()}
+					>
+						{section.split(" ").map((word) => (
+							<div
+								key={generateKey()}
+								className="overflow-wrapper-2 relative about-word"
+							>
+								{(() => {
+									if (word === "Samridh") return <span>Samridh</span>;
+									if (word === "creative")
+										return (
+											<GradientText
+												colors={[
+													"#ffbe0b",
+													"#fb5607",
+													"#ff006e",
+													"#8338ec",
+													"#3a86ff",
+												]}
+												animationSpeed={3}
+												showBorder={false}
+											>
+												Creative
+											</GradientText>
+										);
+									if (word === "Innovative") return <Cover>Innovative</Cover>;
+									if (word === "Solutions")
+										return <span className="text-solutions">Solutions</span>;
 
-								return <span>{word}</span>;
-							})()} */}
-						</div>
-					))}
+									return <span className="block">{word}</span>;
+								})()}
+							</div>
+						))}
+					</section>
+				))}
 			</div>
 		</section>
 	);
